8c4a4a2f1a60f83332c019140480d53fd15ee73a
cheongnowle <iljin10637@naver.com>
2020-09-17 14:09:03 +0900

    2.0 making server with expressJS

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+const POST = 4000;
+
+function handleListening() {
+  console.log(`Listening on : https://localhost:${POST}`);
+}
+
+const handleHome = (req, res) => {
+  res.send("Hi, from home!!");
+};
+const handleProfile = (req, res) => {
+  res.send("You are on my Profile!");
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(POST, handleListening);
