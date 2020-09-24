caecfd4494e86a3366522f21e5d2a0b185934f1c
cheongnowle <iljin10637@naver.com>
2020-09-24 10:16:19 +0900

    2.18 Search controller

---
+++ controllers/videoController.js
@@ -0,0 +1,22 @@
+export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("Search", { pageTitle: "Search", searchingBy });
+};
+
+export const videos = (req, res) =>
+  res.render("Videos", { pageTitle: "Videos" });
+
+export const upload = (req, res) =>
+  res.render("Upload", { pageTitle: "Upload" });
+
+export const videoDetail = (req, res) =>
+  res.render("Video detail", { pageTitle: "Video detail" });
+
+export const editVideo = (req, res) =>
+  res.render("Edit video", { pageTitle: "Edit video" });
+
+export const deleteVideo = (req, res) =>
+  res.render("Delete video", { pageTitle: "Delete video" });
